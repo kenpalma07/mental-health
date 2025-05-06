0ab50f0c93eca2cf0f251a5b9b951ed5d827c5a3
Jamaica Nikki Villamor <maicanikki777@gmail.com>
2025-05-06 14:18:09 +0800

    adding minor updates forms

--- Modules/References/app/Http/Controllers/SetupController.php
+++ Modules/References/app/Http/Controllers/SetupController.php
@@ -6,12 +6,18 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Modules\References\Models\Setup;
 use Carbon\Carbon;
+use Modules\References\Models\FacilitySetup;
 
 class SetupController extends Controller
 {
     public function index()
     {
         return inertia('References::Setup/index');
+        // $setup = FacilitySetup::with('fhudFacility')->first();
+
+        // return Inertia::render('Setup/index', [
+        //     'existingSetup' => $setup
+        // ]);
     }
 }
 
--- Modules/References/routes/web.php
+++ Modules/References/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request; // ✅ this is correct
 use Modules\References\Http\Controllers\ReferencesController;
 use Modules\References\Http\Controllers\FHUDController;
 use Modules\References\Http\Controllers\SetupController;
+use Modules\References\Models\FhudFacility;
 
 Route::get('/references', [ReferencesController::class, 'index']);
 
@@ -12,5 +14,16 @@ Route::get('/references/fhud/create', [FHUDController::class, 'create'])->name('
 Route::post('/references/fhud', [FHUDController::class, 'store'])->name('fhud.store');
 
 Route::get('/references/setup', [SetupController::class, 'index'])->name('facilitysetup');
+
+Route::get('/api/fhud-codes', function (Request $request) {
+    $search = $request->get('search');
+
+    return FhudFacility::when($search, fn($q) =>
+        $q->where('fhudcode', 'like', "%{$search}%")
+          ->orWhere('facility_name', 'like', "%{$search}%")
+    )
+    ->limit(10)
+    ->get(['id', 'fhudcode', 'facility_name', 'faccode',]);
+});
 
 ?>
