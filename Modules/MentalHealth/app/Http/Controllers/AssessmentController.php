9679630c3e1b4c10cc342b8b54bad1318db8e616
DOHRO13 <casipongkenneth@gmail.com>
2025-05-20 14:33:22 +0800

    add error resolvea

--- Modules/MentalHealth/app/Http/Controllers/AssessmentController.php
+++ Modules/MentalHealth/app/Http/Controllers/AssessmentController.php
@@ -228,7 +228,7 @@ class AssessmentController extends Controller
 
         return response()->json([
             'success' => true,
-            'redirect_url' => route('patitrforms.index', $request->pat_temp_id),
-        ]);
+            'redirect_url' => route('patitrforms.index', $request->pat_temp_id) . '?consult_date=' . $request->consult_date,
+        ]);   
     }
 }
